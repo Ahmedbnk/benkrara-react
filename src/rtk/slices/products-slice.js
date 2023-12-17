32d12173c4e7914b8ba6600ebb675205a280c236
Ahmedbnk <145155073+Ahmedbnk@users.noreply.github.com>
2023-12-17 21:01:47 +0100

    Update products-slice.js

--- src/rtk/slices/products-slice.js
+++ src/rtk/slices/products-slice.js
@@ -3,22 +3,43 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 export const fetchProducts = createAsyncThunk(
   "productsSlice/fetchProducts",
   async () => {
-    const response = await fetch("https://fakestoreapi.com/products");
-    const data = await response.json();
-    return data;
+    try {
+      const response = await fetch("https://fakestoreapi.com/products");
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error("Error fetching products:", error);
+      throw error;
+    }
   }
 );
 
 const productsSlice = createSlice({
-  initialState: [],
   name: "productsSlice",
+  initialState: {
+    products: [],
+    status: "idle", // "idle" | "loading" | "succeeded" | "failed"
+    error: null,
+  },
   reducers: {},
   extraReducers: (builder) => {
-    builder.addCase(fetchProducts.fulfilled, (state, action) => {
-      return action.payload;
-    });
+    builder
+      .addCase(fetchProducts.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(fetchProducts.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.products = action.payload;
+      })
+      .addCase(fetchProducts.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.error.message;
+      });
   },
 });
 
-export const {} = productsSlice.actions;
 export default productsSlice.reducer;
